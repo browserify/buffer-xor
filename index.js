5a44f8883c021f466ea83c4cbcf05496fa9bdd1d
Dũng Trần <tad88.dev@gmail.com>
2017-09-23 22:34:35 +0700

    Fix out of bounds

--- index.js
+++ index.js
@@ -5,11 +5,11 @@ module.exports = function xor (a, b) {
   var max = Math.max(a.length, b.length)
   var buffer = Buffer.allocUnsafe(max)
 
-  for (var i = 0; i <= min; ++i) {
+  for (var i = 0; i < min; ++i) {
     buffer[i] = a[i] ^ b[i]
   }
 
-  ((a.length > b.length) ? a : b).copy(buffer, min, max)
+  ((a.length > b.length) ? a : b).copy(buffer, min, min, max)
 
   return buffer
 }

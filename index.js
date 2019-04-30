b7715a491fb299127d2fa7380125eb4722529fa3
Chiro Hiro <chiro@fkguru.com>
2019-04-30 23:35:23 +0700

    Support older ECMA

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 var Buffer = require('safe-buffer').Buffer
 
 module.exports = function xor (a, b) {
-  var [min, max] = (a.length >= b.length) ? [b, a] : [a, b]
-  var buffer = Buffer.allocUnsafe(max.length)
-  var i
+  var min, max, buffer, i
+
+  if (a.length >= b.length) {
+    max = a
+    min = b
+  } else {
+    max = b
+    min = a
+  }
+
+  buffer = Buffer.allocUnsafe(max.length)
+
   for (i = 0; i < min.length; i++) {
     buffer[i] = min[i] ^ max[i]
   }
